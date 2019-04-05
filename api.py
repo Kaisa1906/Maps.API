5018dc7d20a409180adba79871472b11c47568ac
EluciferE <45201676+EluciferE@users.noreply.github.com>
2019-04-05 17:15:17 +0500

    123321

--- api.py
+++ api.py
@@ -2,17 +2,17 @@ import sys
 import requests
 import pygame
 import os
-from PyQt5 import uic
+import PyQt5
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QInputDialog, QHBoxLayout, QLabel
 
-
+print(dir(PyQt5))
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('untitled.ui', self)
-        self.btn.clicked.connect(self.clickonjpg)
+        PyQt5.uic.loadUi('untitled.ui', self)
+        self.btn.clicked.connect(self.fun)
 
-    def clickonjpg(self):
+    def fun(self):
         try:
             toponym_longitude = self.x.text()
             toponym_lattitude = self.y.text()
@@ -47,13 +47,61 @@ class MyWidget(QMainWindow):
             # Переключаем экран и ждем закрытия окна.
             pygame.display.flip()
             while pygame.event.wait().type != pygame.QUIT:
-                pass
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_UP:
+                            delta = str(int(delta + 0.005))
+                            # Собираем параметры для запроса к StaticMapsAPI:
+                            map_params = {
+                                "ll": ",".join([toponym_longitude, toponym_lattitude]),
+                                "spn": ",".join([delta, delta]),
+                                "l": "map"
+                            }
+                            print(map_params)
+                            map_api_server = "http://static-maps.yandex.ru/1.x/"
+                            # ... и выполняем запрос
+                            response = requests.get(map_api_server, params=map_params)
+
+                            # Запишем полученное изображение в файл.
+                            map_file = "map.png"
+                            try:
+                                with open(map_file, "wb") as file:
+                                    file.write(response.content)
+                            except IOError as ex:
+                                print("Ошибка записи временного файла:", ex)
+                                sys.exit(2)
+                                screen.blit(pygame.image.load(map_file), (0, 0))
+                        if event.key == pygame.K_DOWN:
+                            delta = str(int(delta - 0.005))
+                            # Собираем параметры для запроса к StaticMapsAPI:
+                            map_params = {
+                                "ll": ",".join([toponym_longitude, toponym_lattitude]),
+                                "spn": ",".join([delta, delta]),
+                                "l": "map"
+                            }
+                            print(map_params)
+                            map_api_server = "http://static-maps.yandex.ru/1.x/"
+                            # ... и выполняем запрос
+                            response = requests.get(map_api_server, params=map_params)
+
+                            # Запишем полученное изображение в файл.
+                            map_file = "map.png"
+                            try:
+                                with open(map_file, "wb") as file:
+                                    file.write(response.content)
+                            except IOError as ex:
+                                print("Ошибка записи временного файла:", ex)
+                                sys.exit(2)
+                                screen.blit(pygame.image.load(map_file), (0, 0))
             pygame.quit()
 
             # Удаляем за собой файл с изображением.
             os.remove(map_file)
         except Exception:
             pass
+
 app = QApplication(sys.argv)
 ex = MyWidget()
 ex.show()
