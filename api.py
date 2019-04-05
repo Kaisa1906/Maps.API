7775375067375e22e3cb9abab7ff14523ab0ff02
Kaisa1906 <45201804+Kaisa1906@users.noreply.github.com>
2019-04-05 17:51:43 +0500

    Add files via upload

--- api.py
+++ api.py
@@ -2,14 +2,13 @@ import sys
 import requests
 import pygame
 import os
-import PyQt5
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QInputDialog, QHBoxLayout, QLabel
 
-print(dir(PyQt5))
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        PyQt5.uic.loadUi('untitled.ui', self)
+        uic.loadUi('untitled.ui', self)
         self.btn.clicked.connect(self.fun)
 
     def fun(self):
@@ -46,55 +45,39 @@ class MyWidget(QMainWindow):
             screen.blit(pygame.image.load(map_file), (0, 0))
             # Переключаем экран и ждем закрытия окна.
             pygame.display.flip()
-            while pygame.event.wait().type != pygame.QUIT:
+            running = True
+            while running:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         running = False
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_UP:
-                            delta = str(int(delta + 0.005))
-                            # Собираем параметры для запроса к StaticMapsAPI:
-                            map_params = {
-                                "ll": ",".join([toponym_longitude, toponym_lattitude]),
-                                "spn": ",".join([delta, delta]),
-                                "l": "map"
-                            }
-                            print(map_params)
-                            map_api_server = "http://static-maps.yandex.ru/1.x/"
-                            # ... и выполняем запрос
-                            response = requests.get(map_api_server, params=map_params)
-
-                            # Запишем полученное изображение в файл.
-                            map_file = "map.png"
-                            try:
-                                with open(map_file, "wb") as file:
-                                    file.write(response.content)
-                            except IOError as ex:
-                                print("Ошибка записи временного файла:", ex)
-                                sys.exit(2)
-                                screen.blit(pygame.image.load(map_file), (0, 0))
+                            if float(delta) < 45:
+                                delta = str(float(delta)*2)
                         if event.key == pygame.K_DOWN:
-                            delta = str(int(delta - 0.005))
-                            # Собираем параметры для запроса к StaticMapsAPI:
-                            map_params = {
+                            delta = str(float(delta)/2)
+                        map_params = {
                                 "ll": ",".join([toponym_longitude, toponym_lattitude]),
                                 "spn": ",".join([delta, delta]),
                                 "l": "map"
                             }
-                            print(map_params)
-                            map_api_server = "http://static-maps.yandex.ru/1.x/"
-                            # ... и выполняем запрос
-                            response = requests.get(map_api_server, params=map_params)
+                        print(map_params)
+                        map_api_server = "http://static-maps.yandex.ru/1.x/"
+                        # ... и выполняем запрос
+                        response = requests.get(map_api_server, params=map_params)
 
-                            # Запишем полученное изображение в файл.
-                            map_file = "map.png"
-                            try:
-                                with open(map_file, "wb") as file:
-                                    file.write(response.content)
-                            except IOError as ex:
-                                print("Ошибка записи временного файла:", ex)
-                                sys.exit(2)
-                                screen.blit(pygame.image.load(map_file), (0, 0))
+                        # Запишем полученное изображение в файл.
+                        map_file = "map.png"
+                        try:
+                            with open(map_file, "wb") as file:
+                                file.write(response.content)
+                        except IOError as ex:
+                            print("Ошибка записи временного файла:", ex)
+                            sys.exit(2)
+                            screen.blit(pygame.image.load(map_file), (0, 0))
+                        screen.blit(pygame.image.load(map_file), (0, 0))
+                        pygame.display.flip()
+                        
             pygame.quit()
 
             # Удаляем за собой файл с изображением.
